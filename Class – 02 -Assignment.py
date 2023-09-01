ac130fb270fc9f1fb8b846809da02b409cf9f864
Shawon Smith <114006558+smithitcompany@users.noreply.github.com>
2023-09-02 00:21:38 +0600

    Add files via upload

---
+++ Class – 02 -Assignment.py
@@ -0,0 +1,83 @@
+# Class – 02 -Assignment 01
+# print your name
+print("Shawon Smith")
+
+# --------------------------
+# Class – 02 -Assignment 02
+# Input your batch number using input() function.
+Batch = input("Enter Your Batch Number Please:", )
+print("Your Batch Number Is: " + Batch)
+
+# -----------------------------------
+# Class – 02 -Assignment 03
+# Create 5 different valid Variables and print.
+Name = "Shawon Smith"
+Age = "I am 16"
+
+GPA = 5.00
+# Format GPA to display two decimal places
+GPA = "{:.2f}".format(GPA)
+
+Height = "165CM"
+Weight = "70KG"
+
+print("My Name is:", Name)
+print(Age)
+print("My GPA is:", GPA)
+print("My Height is:", Height)
+print("My Weight is:", Weight)
+
+# -------------------------------
+# Class – 02 -Assignment 04
+# Create Pascal Case Variable.
+WhatIsYourName = "My name is Shawon Smith"
+print(WhatIsYourName)
+
+# ------------------------------
+# Class – 02 -Assignment 05
+# Assign a single value to Multiple variables.
+a = b = c = d = 50.5
+print(a)
+print(b)
+print(c)
+print(d)
+
+# ---------------------------
+# Class – 02 -Assignment 06
+# Input float value using input().
+A = float(input("Enter a float value: "))
+print("The Float Value Is:", A)
+
+# -------------------------------------
+# Class – 02 -Assignment 07
+# a = ‘Welcome to Django for web and Ai’ convert into upper case.
+a = 'welcome to Django for web and Ai'
+print(a.upper())
+
+# ----------------------------
+# Class – 02 -Assignment 08
+# b = ‘        Python’remove whitespace.
+b = '            python'
+print(b.strip)
+
+# -------------------------
+# Class – 02 -Assignment 09
+# c = 60, d= 22 ‘Conversion int to float’.
+c = 60
+d = 22
+print("Float value of c:", float(c))
+print("Float value of d:", float(d))
+
+# ---------------------------------
+# Class – 02 -Assignment 10
+# Create a dictionary & print your info.
+My_Dictinory = {'Name': 'Shawon Smith',
+                'Age': 16,
+                'Height': 5.6,
+                'Weight': '70KG'}
+# print(My_Dictinory) or
+print('My Info:')
+print('My Name is', My_Dictinory['Name'])
+print('My Age is', My_Dictinory['Age'])
+print('My Height is', My_Dictinory['Height'])
+print('My Weight is', My_Dictinory['Weight'])
